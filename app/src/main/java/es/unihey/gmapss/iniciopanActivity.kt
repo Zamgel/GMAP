fe776566fe2c1ccb6a376cd92bcaea7e33cb1074
gmaps <jato02@outlook.com>
2024-05-11 14:06:06 -0600

    Se agrega el proyecto

---
+++ app/src/main/java/es/unihey/gmapss/iniciopanActivity.kt
@@ -0,0 +1,34 @@
+package es.unihey.gmapss
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.animation.AnimationUtils
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+
+class iniciopanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_iniciopan)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+
+        val perd= findViewById<CardView>(R.id.perd)
+        perd.setOnClickListener {
+            val animation = AnimationUtils.loadAnimation(applicationContext, R.anim.scale_animation)
+            perd.startAnimation(animation)
+           
+        }
+    }
+}
